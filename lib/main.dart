2636cb4164d5d11ac242ea50d3e6f3d5270ad4ff
suad-krvavac <suad.krvavac@gmail.com>
2022-06-30 20:21:43 +0200

    3. Column alignent...

--- lib/main.dart
+++ lib/main.dart
@@ -35,19 +35,22 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter app')),
+      appBar: AppBar(title: const Text('Flutter app')),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
-            alignment: Alignment.center,
-            child: Card(
+            // alignment: Alignment.center,
+            child: const Card(
               color: Colors.blue,
               child: Text('CHART!'),
               elevation: 5,
             ),
           ),
           Card(
+            color: Colors.red,
             child: Text('LIST OF TX'),
           )
         ],
